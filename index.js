59026f1cc7f4b2c4816dff58caf8d46fee11ac36
SDBoahen <SDBoahen@gmail.com>
2023-06-02 11:29:28 -0400

    Added src + helpers.js + data + adnimals/json

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("^_^")
+const { readJSONFile, writeJSONFile } = require("./src/helpers")
+
+
+function run(){
+
+    console.log("Welcome to Our Animals App! 🐢✨")
+
+}
+
+run()
---
+++ src/helpers.js
@@ -0,0 +1,27 @@
+// const fs = require("node:fs")
+
+const { readFileSync, writeFileSync } = require("node:fs")
+
+
+
+
+function readJSONFile(path, fileName){
+
+    const object = readFileSync(`${path}/${fileName}`, "utf-8")
+    return object ? JSON.parse(object) : []
+}
+
+
+function writeJSONFile(path, fileName, data){
+
+    data = JSON.stringify(data)
+    return writeFileSync(`${path}/${fileName}`, data, { encoding: "utf-8" })
+}
+
+
+
+
+module.exports = {
+    readJSONFile, 
+    writeJSONFile
+}
